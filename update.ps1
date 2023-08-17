835129ccb9fdb7cc5c1afaaabecdfd421ead42e9
Mister_Bert0ni <corpofhack@gmail.com>
2023-08-17 13:44:18 +0300

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -URI 'https://github.com/NotSoEthical/lolk3k/blob/main/4.png?raw=true' -OutFile C:\Ransomware.png 
 echo 'Ransomware Simulation was successfully done by Internal EPAM Red Team. Happy Investigation :))'> C:\ransomware.txt
-msg * "Oops, it seems your computer has been targeted by ransomware attack! Don't warry it is simulation by Internal EPAM Red Team!"
+msg * "Oops, it seems your computer has been targeted by ransomware attack!"
 
