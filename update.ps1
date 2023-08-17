81793b788d1f08b86122bcb7a9874b66e3844d0e
Mister_Bert0ni <corpofhack@gmail.com>
2023-08-17 13:48:07 +0300

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,4 +1,6 @@
 Invoke-WebRequest -URI 'https://github.com/NotSoEthical/lolk3k/blob/main/4.png?raw=true' -OutFile C:\Ransomware.png 
 echo 'Ransomware Simulation was successfully done by Internal EPAM Red Team. Happy Investigation :))'> C:\ransomware.txt
-msg * "Oops, it seems your computer has been targeted by ransomware attack!"
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$msgBody = "Oops! It seems your computers has been targeted by ransomware attack! Don't worry it is simulation by Internal EPAM Red Team, please check C:\ransomware.png file and report to security team! "
+[System.Windows.MessageBox]::Show($msgBody)
 
